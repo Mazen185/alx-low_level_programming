6390b24381dd084865ce455404ab865d9c27ceae
Mazen Mahmoud <mazenxmazen22@gmail.com>
2023-02-22 12:14:23 -0800

    im the best

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include main.h
+
+/**
+ * main - printing "_putchar" with putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
